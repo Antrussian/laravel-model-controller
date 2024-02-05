6a4c3dddf06935f7c0c4098b98fa08df55c1412d
Antrussian <antonino.russo.agent@gmail.com>
2024-02-05 02:20:01 +0100

    Accesso al db

--- app/Http/Controllers/Guest/MovieController.php
+++ app/Http/Controllers/Guest/MovieController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
     //
 
     public function index(){
-        return view('guest.movie.index');
+        $movies = Movie::all();
+
+        dd($movies);
+
+        return view('guest.movie.index', compact('movies'));
     }
 }
